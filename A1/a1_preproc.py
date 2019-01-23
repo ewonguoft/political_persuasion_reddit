551198413c96194df2eb1fd7b7412395538b5e54
Wong Elton Goon Hin <eltong.wong@mail.utoronto.ca>
2019-01-22 22:43:10 -0500

    fixing bug with extra /

--- A1/a1_preproc.py
+++ A1/a1_preproc.py
@@ -118,7 +118,7 @@ def preproc1( comment , steps=range(1,11)):
                 if(token[0] != "-" and i.lemma_[0] == "-"):
                     tokens_mod.append(word)
                 else:
-                    tokens_mod.append("{0}/{1}".format(i.lemma_, tag))
+                    tokens_mod.append("{0}{1}".format(i.lemma_, tag))
 
         modComm = " ".join(tokens_mod)
 
